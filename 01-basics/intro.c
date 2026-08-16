fa9ececdabbb0814ebfd04aa0d9907b39bcbb143
Subham Nayak <81096401+subhu2008@users.noreply.github.com>
2026-08-16 20:28:55 +0000

    HELL YEAH, intro.c IS NOW FINALLY CLEAN TOO

---
+++ 01-basics/intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) // There is absolutely no way i wrote "viod" instead of "void" cuz of this crap keyboard, anyways corrected it after reviewing this code again //
+{
+    printf("Yo me subhu2008, and unemployed guy but still learning\n");
+    printf("Just hoping i can learn C under less time cuz why not\n");
+    printf("I just wanted to learn C randomly from scratch cuz of stuffs i have been doing lately, else there is absolutely no point of knowing stuffs without understanding the foundations\n");
+    printf("If i never know the foundation, it would be just be nothing but a larp even if you do have some basic understanding\n");
+    printf("So this was all what i wanted to say in this intro.c, let's see if this code goes successfull (hoping so)\n");
+    printf("Btw, when i was just writing this, i forgot to include semicolon until i realised that code would be fucked without semicolon lol\n");
+    printf("My goal is to keep the code duct-tape free, tho i may mess up later since i'm still learning lol, literally 2 am learning as of now\n");
+    return 0;
+}
