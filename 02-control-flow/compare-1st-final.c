49816b75e6e051cdad146b24c59d88c775eaecc8
Subham Nayak <81096401+subhu2008@users.noreply.github.com>
2026-08-17 10:22:26 +0000

    So yes, i finally removed that one toxic ex gf of mine (semicolon) which was ending the statement without the x int condition, so even if x was smaller than the digit, it always said that it's greater, that is exactly how my ex gf used to lie

---
+++ 02-control-flow/compare-1st-final.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// oops, made a mistake. forgot to include the x as an int //
+
+int main(void)
+{
+    int x = 10;
+
+if (x > 5) // Finally removed the semicolon which was def like my ex gf, manipulative asf. Atleast removing the semicolon should do the job //
+// In this exact line, i forgot to remove the "{" bracket, which was causing gcc to freak out, atleast now i learned what exactly was the error, now corrected //
+    printf("x is great\n"); // Lmao forgot to include semicolon, atleast now i did //
+    return 0; // Damn forgot this one too, atleast i remembered now //
+}
