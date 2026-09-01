649b3df803ef674a566707783e1f0b4889273fd9
Subham Nayak <81096401+subhu2008@users.noreply.github.com>
2026-09-01 07:03:28 +0000

    Learn else if with extra infos just to stress test my memory

---
+++ 02-control-flow/else-if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10;
+
+if (x > 5)
+
+    printf("x is greater than 5");
+
+else if (x == 10)
+
+    printf("x is exactly 10");
+
+else if (x < 10)
+
+    printf("10 is greater than x");
+
+else
+
+   printf("x is smaller than 5");
+   return 0;
+}
